cf2c6cb6f0b1e2b1fb80d49f622b7858f49cf075
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-07-27 05:30:37 +0900

    Add files via upload
    여러 공식

---
+++ code_1.R
@@ -0,0 +1,68 @@
+pi
+print(pi, digits = 4)
+
+x <- 5
+(x > 3)
+(x > 6)
+
+(3 == 4)
+!(x > 3)
+(x > 3)|(x > 6)
+(x<3)&(x>6)
+
+sin(pi)
+(cos(pi/3)-sqrt(2))/exp(4)
+
+asin(1/2)
+atan(1)
+x=pi/2
+cospu(x)
+cos(pi*x)
+
+log(2)
+log(16,2) #log(x, base)
+log10(10)
+log(10) #log(10,e)
+log1p(0.3) #log(1+x), for |x|<1
+
+x <- c(3,2,5,4,3,2,8,9,12)
+mean(x)
+max(x)
+min(x)
+median(x)
+sd(x)
+var(x)
+boxplot(x)
+
+#d p q r??? ??????
+dnorm(0)
+pnorm(-1.645)
+qnorm(0.05)
+rnorm(10)
+
+dt(0,7)
+pt(-1.645,7)
+qt(0.05,7)
+rt(10,7)
+
+#?????? ???????????? ?????????
+x <- c(3,2,5,4,3,2,8,9,12)
+name <- c("daehak")
+
+ls()
+cat("code_05.R \n")
+
+print("i love r coding")
+cat("\n")
+
+tolower(name)
+toupper(name)
+
+class(x)
+class(name)
+
+length(x)
+length(name)
+
+rm(x)
+ls()
